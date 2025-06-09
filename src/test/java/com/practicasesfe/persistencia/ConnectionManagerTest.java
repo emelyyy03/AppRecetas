d7995570702d6f1b510ba9cc401367ea23f42d89
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-09 10:23:49 -0600

    Se creo la clase de Test de conexion y el archivo POM

---
+++ src/test/java/com/practicasesfe/persistencia/ConnectionManagerTest.java
@@ -0,0 +1,17 @@
+package com.practicasesfe.persistencia;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection; // Importa la clase Connection del paquete java.sql, que se utiliza para establecer una conexión con la base de datos.
+import java.sql.SQLException; // Importa la clase SQLException del paquete java.sql, que se utiliza para manejar excepciones relacionadas con operaciones de base de datos.
+
+import static org.junit.jupiter.api.Assertions.*; // Importa todos los métodos estáticos de la clase Assertions del paquete org.junit.jupiter.api. Esto proporciona métodos para realizar aserciones en las pruebas unitarias, como assertEquals, assertTrue, etc.
+
+
+class ConnectionManagerTest {
+
+    void connect() {
+    }
+}
