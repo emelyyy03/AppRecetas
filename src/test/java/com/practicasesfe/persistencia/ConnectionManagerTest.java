08a7475f0ea3cffab48bdbfeb941c56d138a5561
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-09 10:29:47 -0600

    Se conecto a la base de datos en SQL Server

--- src/test/java/com/practicasesfe/persistencia/ConnectionManagerTest.java
+++ src/test/java/com/practicasesfe/persistencia/ConnectionManagerTest.java
@@ -11,7 +11,38 @@ import static org.junit.jupiter.api.Assertions.*; // Importa todos los métodos 
 
 
 class ConnectionManagerTest {
+    ConnectionManager connectionManager;
+    @BeforeEach
+    void setUp() throws SQLException {
+        // Se ejecuta antes de cada método de prueba.
+        // Inicializa el ConnectionManager.
+        connectionManager = ConnectionManager.getInstance();
+    }
 
-    void connect() {
+    @AfterEach
+    void tearDown() throws SQLException {
+        // Se ejecuta después de cada método de prueba.
+        // Cierra la conexión y limpia los recursos.
+        if (connectionManager != null) {
+            connectionManager.disconnect();
+            connectionManager = null; // Para asegurar que no se use accidentalmente
+        }
+    }
+
+
+    @Test
+    void connect() throws SQLException{
+        // Intenta establecer una conexión a la base de datos utilizando el método connect() de ConnectionManager.
+        Connection conn = connectionManager.connect();
+        // Realiza una aserción para verificar que la conexión establecida no sea nula.
+        // Si conn es nulo, la prueba fallará con el mensaje "La conexion no debe ser nula".
+        assertNotNull(conn, "La conexion no debe ser nula");
+        // Realiza una aserción para verificar que la conexión establecida esté abierta.
+        // El método isClosed() devuelve true si la conexión está cerrada, por lo que assertFalse espera que devuelva false.
+        // Si la conexión está cerrada, la prueba fallará con el mensaje "La conexion debe esta abierta".
+        assertFalse(conn.isClosed(), "La conexion debe esta abierta");
+        if (conn != null) {
+            conn.close(); // Cierra la conexión después de la prueba.
+        }
     }
 }
