6b683d903c26c7d54e29b51507d88f5c50a4d5a5
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-10 09:57:56 -0600

    Se edito la clase CicloDAOTest

--- src/test/java/com/practicasesfe/persistencia/CicloDAOTest.java
+++ src/test/java/com/practicasesfe/persistencia/CicloDAOTest.java
@@ -1,5 +1,6 @@
 package com.practicasesfe.persistencia;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.sql.Date;
 import java.util.List;
@@ -7,6 +8,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CicloDAOTest {
+    private CicloDAO cicloDAO; // Instancia de la clase UserDAO que se va a probar.
+
+    @BeforeEach
+    void setUp(){
+        // Método que se ejecuta antes de cada método de prueba (@Test).
+        // Su propósito es inicializar el entorno de prueba, en este caso,
+        // creando una nueva instancia de UserDAO para cada prueba.
+            cicloDAO = new CicloDAO();
+    }
 
     @Test
     void insertar() {
