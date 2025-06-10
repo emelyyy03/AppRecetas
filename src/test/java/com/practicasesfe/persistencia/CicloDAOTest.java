18c2f5742568d4a8aa992bb7814987d130c303f2
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-06-09 22:30:30 -0600

    Se agrego la clase CicloDAOTest

---
+++ src/test/java/com/practicasesfe/persistencia/CicloDAOTest.java
@@ -0,0 +1,14 @@
+package com.practicasesfe.persistencia;
+
+import org.junit.jupiter.api.Test;
+import java.sql.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CicloDAOTest {
+
+    @Test
+    void insertar() {
+    }
+}
